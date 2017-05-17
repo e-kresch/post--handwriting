f9f70835713dfcf8c24dca6729d7093e727a0aa3
Erica Kresch <ekresch15@gmail.com>
2017-05-16 22:42:34 -0400

    time delay

--- public/assets/worker.js
+++ public/assets/worker.js
@@ -4,6 +4,13 @@ var penState, x, y, dx, dy;
 var model_x, model_s, model_z;
 var temperature = 0.65;
 
+restart();
+
+onmessage = function(e) {
+  if (e.data.key == "temperature") temperature = e.data.value;
+  if (e.data.key === "step") step();
+}
+
 function restart() {
   x = 0;
   y = 0;
@@ -23,23 +30,7 @@ function step() {
   dy = model_x.get(1);
   penState = model_x.get(2);
 
-  // Get an array out from the worker since nd class doesn't transfer.
-  // var model_z_out = model_z.map(function(d) {
-  //   var a = [];
-  //   for (var i = 0; i < 20; i++) {
-  //     a.push(d.get(i));
-  //   }
-  //   return a;
-  // });
 
-  // var samples = [];
-  // for (var i = 0; i < 200; i++) {
-  //   var m = Model.sample(model_z);
-  //   var sx = m.get(0);
-  //   var sy = m.get(1);
-  //   var sp = m.get(2);
-  //   samples.push([sx, sy, sp]);
-  // }
 
   postMessage([dx, dy, penState, x, y]);
   x += dx;
@@ -47,11 +38,4 @@ function step() {
   if (x > 150) {
     restart();
   }
-}
-
-restart();
-
-onmessage = function(e) {
-  if (e.data.key == "temperature") temperature = e.data.value;
-  if (e.data.key === "step") step();
 }
